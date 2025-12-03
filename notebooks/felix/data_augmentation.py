c686c10d469f5203245cb07b58a373f040cd5e7b
abir boualaoui <abir.boualaoui@gmail.com>
2025-12-03 10:21:07 +0100

    renamed catg-classifier-finetuned and added catg-classifier-Inception

--- notebooks/felix/data_augmentation.py
+++ notebooks/felix/data_augmentation.py
@@ -8,6 +8,9 @@ Created on Tue Dec  2 14:55:02 2025
 import tensorflow as tf
 from tensorflow.keras import layers, Sequential
 from tensorflow.keras.layers import Input
+from tensorflow.keras.metrics import Recall
+from keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
 
 def initialize_model_with_aug():
     '''Instanciate and return the CNN architecture with light data augmentation'''
@@ -58,8 +61,6 @@ def initialize_model_with_aug():
     return model
 
 
-from tensorflow.keras.metrics import Recall
-
 def compile_model(model):
     '''return a compiled model suited for the CIFAR-10 task'''
     model.compile(optimizer= 'adam',
@@ -72,9 +73,9 @@ model = initialize_model_with_aug()  # <-- utiliser la version avec augmentation
 model.summary()
 
 model = compile_model(model)
-es = EarlyStopping(patience=3, restore_best_weights=True)
+es = EarlyStopping(patience=3, restore_best_weights=True)  # pyright: ignore[reportUndefinedVariable]
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)  # pyright: ignore[reportUndefinedVariable]
 
 history = model.fit(X_train, y_train,
                     epochs=10,
